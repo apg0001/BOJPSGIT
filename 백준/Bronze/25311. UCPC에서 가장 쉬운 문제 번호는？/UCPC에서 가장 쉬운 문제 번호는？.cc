14b21929bf1ace50126ca75cd4eb3b5f49978650
박기찬 <apg0001naver@gmail.com>
2023-05-29 19:27:18 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int y;
+    cin >> y;
+    cout << "A" << endl;
+    return 0;
+}
