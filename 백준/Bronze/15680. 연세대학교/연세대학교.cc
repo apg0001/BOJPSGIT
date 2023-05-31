0e1ac79bd98173ca1c10c14b73ce148f9cc66917
박기찬 <apg0001naver@gmail.com>
2023-05-31 22:21:37 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    if (n)
+    {
+        cout << "Leading the Way to the Future" << endl;
+    }
+    else
+    {
+        cout << "YONSEI" << endl;
+    }
+    return 0;
+}
